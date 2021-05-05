5c396862081058fde7552d4344bd688f367c5b5b
Ned Zimmerman <ned@bight.dev>
2021-05-05 16:28:10 -0300

    fix: move GitHub link generator into 11ty data

--- src/documents/documents.11tydata.js
+++ src/documents/documents.11tydata.js
@@ -3,6 +3,14 @@ module.exports = {
     eleventyComputed: {
         permalink: (data) => {
             return "/infusion/development" + data.page.filePathStem.replace("/documents", "") + ".html";
+        },
+        githubLocation: (data) => {
+            // The documentation root on GitHub:
+            // Used to build URLs for "Edit on GitHub" links
+            var githubDocRoot = "https://github.com/fluid-project/infusion-docs/blob/main/src";
+            // in case we're on Windows, replace "\" in the path with "/"
+            var relativePath = data.page.filePathStem.replace(/\\/g, "/");
+            return githubDocRoot + relativePath + ".md";
         }
     }
 };
